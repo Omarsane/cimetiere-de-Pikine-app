bfaee4c3605b679fef38a5790c9b6016e9a807cd
omar sane <saneomar638@gmail.com>
2025-08-04 16:47:50 +0200

    Ajout de la structure de base de l'application

--- CIMETIERE.py
+++ CIMETIERE.py
@@ -1,14 +1,28 @@
+# Importation des bibliothèques nécessaires
 import streamlit as st
-#titre centre
+from PIL import Image
+
+# Titre centré de la page
 st.markdown(
     "<h1 style='text-align: center; color: green;'>Gestion du Cimetière Municipal de la Ville de Pikine</h1>",
     unsafe_allow_html=True
 )
 
-st.image("images/cimetiere.tif")
-with st.sidebar:
-    st.info("Sur cette plateforme;vous pouvez rechercher,ajouter ou supprimer un défunt ")
-    
-    st.warning("NB:Seul les gestionnaires du cimetiere ont le droit de supprimer ou d'ajouter un défunt.")
+# Utilisation de Pillow pour ouvrir l'image de manière fiable
+try:
+    image_path = "images/cimetiere.tif"
+    image = Image.open(image_path)
+    st.image(image)
+except FileNotFoundError:
+    st.error("Le fichier images/cimetiere.tif n'a pas été trouvé.")
 
-st.page_link("https://umap.openstreetmap.fr/fr/map/cimetiere-de-piking_1226453?scaleControl=false&miniMap=fa", label="Cliquer ici pour aller vers la carte du Cimetière de Piking")
+# Création de la barre latérale
+with st.sidebar:
+    st.info("Sur cette plateforme, vous pouvez rechercher, ajouter ou supprimer un défunt.")
+    st.warning("NB: Seuls les gestionnaires du cimetière ont le droit de supprimer ou d'ajouter un défunt.")
+
+# Lien vers la carte du cimetière
+st.page_link(
+    "https://umap.openstreetmap.fr/fr/map/cimetiere-de-piking_1226453?scaleControl=false&miniMap=fa", 
+    label="Cliquer ici pour aller vers la carte du Cimetière de Piking"
+)
